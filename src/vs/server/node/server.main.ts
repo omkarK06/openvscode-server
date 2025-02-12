5a5cdd6718b435204a4253b4e88c31122fb93c43
omkark06 <omkar@zinclabs.io>
2025-02-13 02:43:45 +0530

    fix: updated built in extensions path

--- src/vs/server/node/server.main.ts
+++ src/vs/server/node/server.main.ts
@@ -44,10 +44,14 @@ const GLOBAL_STORAGE_HOME = join(APP_SETTINGS_HOME, 'globalStorage');
 const LOCAL_HISTORY_HOME = join(APP_SETTINGS_HOME, 'History');
 const MACHINE_SETTINGS_HOME = join(USER_DATA_PATH, 'Machine');
 const APP_ROOT = dirname(FileAccess.asFileUri('').fsPath);
-const BUILTIN_EXTENSIONS_FOLDER_PATH = join(APP_ROOT, '/extensions');
+const BUILTIN_EXTENSIONS_FOLDER_PATH = join(APP_ROOT, '/builtin-extensions');
 args['user-data-dir'] = USER_DATA_PATH;
 args['builtin-extensions-dir'] = BUILTIN_EXTENSIONS_FOLDER_PATH;
 args['extensions-dir'] = args['extensions-dir'] || join(REMOTE_DATA_FOLDER, 'extensions');
+
+console.log('App Root:', APP_ROOT);
+console.log('extensions path', BUILTIN_EXTENSIONS_FOLDER_PATH);
+
 
 [REMOTE_DATA_FOLDER, args['extensions-dir'], USER_DATA_PATH, APP_SETTINGS_HOME, MACHINE_SETTINGS_HOME, GLOBAL_STORAGE_HOME, LOCAL_HISTORY_HOME].forEach(f => {
 	try {
